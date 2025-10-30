f498eba88c79b26e39d50221b4203849f5117c78
Lahcen <akdiml309@gmail.com>
2025-10-30 16:48:39 +0100

    une version stable de conteur des billet

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const allEvents = document.querySelectorAll(".event");
+var tohide = document.getElementById("content1");
+var toadd1 = document.getElementById("lesbillets");
+let conteur = 0 ;
+// tohide.style.display="none" ;
+
+
+for (const element of allEvents) {
+    element.addEventListener("click",changeborder2);   
+}
 
 function changeborder2 (event){
     
     for (const element of allEvents) {
         element.style.border="3px solid white";
     }
-    event.currentTarget.style.border="3px solid red"
+    event.currentTarget.style.border="3px solid red";
     console.log(event.currentTarget);
 }
 
-for (const element of allEvents) {
-    element.addEventListener("click",changeborder2);
-    
-}
+
+
 var tohide = document.getElementById("content1");
 console.log(tohide)
 var titre = document.getElementById("title");
@@ -20,7 +27,20 @@ console.log(titre)
 
 function hide(){
    tohide.style.display="none" ;
-   title.innerHTML="_______________ Deusiémme étape _______________" ;
+   title.textContent="_______________ Deusiémme étape _______________";
+}
+function toadd(){
+    console.log(conteur)
+    console.log(toadd)
+
+conteur += 1 ;
+toadd1.textContent=conteur;
+}
+function toremove(){
+    if(conteur > 0) {
+    conteur -= 1 ;
+    toadd1.textContent=conteur;
+    }
 }
 
 
